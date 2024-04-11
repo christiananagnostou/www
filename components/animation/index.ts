9cd8819fbd5ade2074d6e27a5fec5ef79a967340
ChristianAnagnostou <christian0722@gmail.com>
2024-04-10 22:33:56 -0700

    speed up homepage animation

--- components/animation/index.ts
+++ components/animation/index.ts
@@ -2,9 +2,9 @@ export const pageAnimation = {
   hidden: { opacity: 0 },
   show: {
     opacity: 1,
-    transition: { duration: 0.25, staggerChildren: 0.15 },
+    transition: { duration: 0.1, staggerChildren: 0.1 },
   },
-  exit: { opacity: 0, transition: { duration: 0.25 } },
+  exit: { opacity: 0, transition: { duration: 0.2 } },
 }
 
 export const fade = {
