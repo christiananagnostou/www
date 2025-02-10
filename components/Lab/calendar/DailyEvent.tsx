4f3d13a33cf0692da7b1053a6e3fbde74d5f6328
ChristianAnagnostou <christian0722@gmail.com>
2025-02-09 21:37:17 -0800

    feat: improve daily calendar

--- components/Lab/calendar/DailyEvent.tsx
+++ components/Lab/calendar/DailyEvent.tsx
@@ -1,51 +1,57 @@
+import { Dayjs } from 'dayjs'
 import X from '../../SVG/X'
 import { DailyEventT } from './DailyCalendar'
 import { DailyEventContainer, DailyEventInner, DailyEventRelative, DeleteButton, ResizeBar, TimeRange } from './styles'
 import { dateToTime, timeToPx } from './utils'
 
+type LayoutStyle = {
+  left: string
+  width: string
+}
+
 type Props = {
   dailyEvent: DailyEventT
   updateDailyEvent?: (dailyEvent: DailyEventT) => void
-  getDateFromPointerEvent?: (e: MouseEvent) => Date
+  pointerEventToDate?: (e: MouseEvent) => Dayjs
   selectedEventId?: number | null
   setSelectedEventId?: React.Dispatch<React.SetStateAction<number | null>>
   deleteDailyEvent?: (dailyEvent: DailyEventT) => void
+  layoutStyle?: LayoutStyle
 }
 
-const getDailyEventBoxStyles = (cursorPos: DailyEventT) => {
-  if (!cursorPos) return {}
-  const { start, end } = cursorPos
-  const startPx = timeToPx(dateToTime(start))
-  const endPx = timeToPx(dateToTime(end))
-  const shouldSwap = startPx > endPx
+const getDailyEventBoxStyles = (event: DailyEventT, layoutStyle?: LayoutStyle) => {
+  const startPx = timeToPx(dateToTime(event.start))
+  const endPx = timeToPx(dateToTime(event.end))
 
   return {
-    height: shouldSwap ? startPx - endPx : endPx - startPx,
-    top: shouldSwap ? endPx : startPx,
-    background: cursorPos.color,
+    height: Math.abs(endPx - startPx),
+    top: Math.min(startPx, endPx),
+    background: event.color,
+    ...layoutStyle,
   }
 }
 
 const DailyEvent = ({
   dailyEvent,
   updateDailyEvent,
-  getDateFromPointerEvent,
+  pointerEventToDate,
   selectedEventId,
   setSelectedEventId,
   deleteDailyEvent,
+  layoutStyle,
 }: Props) => {
   const onResizeDown = (mouseDownEvent: React.MouseEvent<HTMLDivElement>, position: 'start' | 'end') => {
     mouseDownEvent.stopPropagation()
     document.body.style.cursor = 'ns-resize'
 
-    const onMouseMove = (mouseDownEvent: MouseEvent) => {
-      if (!getDateFromPointerEvent) return
+    const onMouseMove = (mouseMoveEvent: MouseEvent) => {
+      if (!pointerEventToDate) return
 
-      const resizedDate = getDateFromPointerEvent(mouseDownEvent)
+      const resizedDate = pointerEventToDate(mouseMoveEvent)
       const { start, end } = dailyEvent
 
-      if (position === 'start' && resizedDate.getTime() === end.getTime()) return
-      if (position === 'end' && resizedDate.getTime() === start.getTime()) return
+      if (position === 'start' && resizedDate.valueOf() === end.valueOf()) return
+      if (position === 'end' && resizedDate.valueOf() === start.valueOf()) return
 
       const resizedEvent = {
         ...dailyEvent,
@@ -74,14 +80,14 @@ const DailyEvent = ({
   return (
     <DailyEventContainer
       className={selectedEventId === dailyEvent.id ? 'selected' : ''}
-      style={getDailyEventBoxStyles(dailyEvent)}
+      style={getDailyEventBoxStyles(dailyEvent, layoutStyle)}
       onClick={onContainerClick}
     >
       <DailyEventRelative>
-        {!(dailyEvent.start.getTime() === dailyEvent.end.getTime()) && (
+        {dailyEvent.start.valueOf() !== dailyEvent.end.valueOf() && (
           <DailyEventInner>
             <TimeRange>
-              {dailyEvent.start < dailyEvent.end
+              {dailyEvent.start.isBefore(dailyEvent.end)
                 ? dateToTime(dailyEvent.start, 12) + ' - ' + dateToTime(dailyEvent.end, 12)
                 : dateToTime(dailyEvent.end, 12) + ' - ' + dateToTime(dailyEvent.start, 12)}
             </TimeRange>
