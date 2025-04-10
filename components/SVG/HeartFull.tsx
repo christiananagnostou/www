fef44dcbc05e052a2acf05314448344c3700d219
ChristianAnagnostou <christian0722@gmail.com>
2025-04-10 14:28:38 -0700

    fix: svg attribute naming

--- components/SVG/HeartFull.tsx
+++ components/SVG/HeartFull.tsx
@@ -3,7 +3,7 @@ const HeartFull = () => {
     <svg
       stroke="currentColor"
       fill="currentColor"
-      stroke-width="0"
+      strokeWidth="0"
       viewBox="0 0 32 32"
       height="1em"
       width="1em"
