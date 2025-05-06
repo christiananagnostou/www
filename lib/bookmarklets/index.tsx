1fe94572bbc9acd3b41437bb7e51b296aea10660
ChristianAnagnostou <christian0722@gmail.com>
2025-05-05 22:03:16 -0700

    feat: remove pokebattle bookmarklet

--- lib/bookmarklets/index.tsx
+++ lib/bookmarklets/index.tsx
@@ -21,41 +21,41 @@ export const bookmarkletsData = [
       2. Click the HotBids bookmarklet.
       3. Enjoy!`,
   },
-  {
-    title: 'PokeBattle',
-    description:
-      'Simplifies the Pokémon Vortex battle interface by hiding unnecessary elements and enlarging buttons for quicker navigation.',
-    icon: Pokeball,
-    code: /*css*/ `
-      javascript:(function(){
-        var style = document.createElement('style');
-        style.innerHTML = \`
-          /* Style buttons for easier clicking */
-          .button-small:not([value="Use Item"]),
-          .menu-tab:first-of-type {
-            width: 100%;
-            background: linear-gradient(#d10101, #900101);
-            padding: 5rem !important;
-            position: fixed;
-            bottom: 0;
-            left: 0;
-            z-index: 999999;
-            border: 5px solid black !important;
-          }
-          #loading {
-            background-image: none !important;
-          }
-        \`;
-        document.head.appendChild(style);
-      })();`,
-    instructions: `
-      Like a true nerd, I've been playing Pokemon Vortex, an OG web-based pokemon game, since the game debuted as Pokemon Crater 2007. To make the game a little easier for those that need *bigger buttons*, this bookmarklet is just what you need. It makes the buttons bigger so that you can fly through battles and get that precious XP.
-      -
-      Instructions:
-      1. Start a battle in Pokemon Vortex.
-      2. Click the PokeBattle bookmarklet.
-      3. The interface will be simplified, hiding unnecessary elements and enlarging buttons for easier clicking.`,
-  },
+  // {
+  //   title: 'PokeBattle',
+  //   description:
+  //     'Simplifies the Pokémon Vortex battle interface by hiding unnecessary elements and enlarging buttons for quicker navigation.',
+  //   icon: Pokeball,
+  //   code: /*css*/ `
+  //     javascript:(function(){
+  //       var style = document.createElement('style');
+  //       style.innerHTML = \`
+  //         /* Style buttons for easier clicking */
+  //         .button-small:not([value="Use Item"]),
+  //         .menu-tab:first-of-type {
+  //           width: 100%;
+  //           background: linear-gradient(#d10101, #900101);
+  //           padding: 5rem !important;
+  //           position: fixed;
+  //           bottom: 0;
+  //           left: 0;
+  //           z-index: 999999;
+  //           border: 5px solid black !important;
+  //         }
+  //         #loading {
+  //           background-image: none !important;
+  //         }
+  //       \`;
+  //       document.head.appendChild(style);
+  //     })();`,
+  //   instructions: `
+  //     Like a true nerd, I've been playing Pokemon Vortex, an OG web-based pokemon game, since the game debuted as Pokemon Crater 2007. To make the game a little easier for those that need *bigger buttons*, this bookmarklet is just what you need. It makes the buttons bigger so that you can fly through battles and get that precious XP.
+  //     -
+  //     Instructions:
+  //     1. Start a battle in Pokemon Vortex.
+  //     2. Click the PokeBattle bookmarklet.
+  //     3. The interface will be simplified, hiding unnecessary elements and enlarging buttons for easier clicking.`,
+  // },
   {
     title: 'GlassBreaker',
     description:
@@ -84,4 +84,5 @@ export const bookmarkletsData = [
       2. Click the Glassdoor Unblocker bookmarklet.
       3. The content wall will be hidden and scrolling will be restored...like it should be`,
   },
+  // Add a bookmarklet that is a bookmarklet hosting widget. This widget should be an expandable and collapsable panel (similar to react-scan's widget) in the bottom right of the screen and when expanded, it should show a list of all the bookmarklets that I make
 ]
